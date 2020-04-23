b9a80ec5c94d3f18867d3da6287c88a574278544
g7tro4w <g7tro4w@gmail.com>
2020-04-23 21:07:32 +0300

    Feature: Button component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Button from '@/components/button'
 
 Vue.config.productionTip = false
+
+Vue.component('ui-button', Button)
 
 new Vue({
   render: h => h(App),
